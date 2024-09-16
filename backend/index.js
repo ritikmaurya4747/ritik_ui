5b1c3712b25a2f0f8c88cb1af0955302d6643566
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-16 18:15:27 +0530

    index.js image url render api

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ app.use('/images',express.static('upload/images'))
 app.post('/upload',upload.single('product'),(req,res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${PORT}/images/${req.file.filename}`
+        image_url: `https://styleboom-c.onrender.com/images/${req.file.filename}`
     })
 })
 
