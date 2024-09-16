264e05532c732e6941ecd9261653401e701f6eb7
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-16 17:53:54 +0530

    port 4000

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { error, log } from 'console';
 const app = express();
 
 dotenv.config();
-const PORT  = process.env.PORT || 3000
+const PORT  = process.env.PORT || 4000
 const MONGODB_URI = process.env.MONGODB_URI
 
 app.use(cors());
