7b7f3964822c65a07e61c0d65d835a9b48cc64b1
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-17 10:42:43 +0530

    upload

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ app.use('/images',express.static('upload/images'))
 app.post('/upload',upload.single('product'),(req,res) => {
     res.json({
         success: 1,
-        image_url: `https://styleboom-c.onrender.com${PORT}/images/${req.file.filename}`
+        image_url: `https://styleboom-c.onrender.com/images/${req.file.filename}`
     })
 })
 
