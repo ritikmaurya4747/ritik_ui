ee02923200d5e36f0969d90c669db42ec128f889
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-18 10:24:38 +0530

    jakajke

--- frontendUi/src/Components/Popular/Popular.jsx
+++ frontendUi/src/Components/Popular/Popular.jsx
@@ -6,10 +6,9 @@ function Popular() {
   const [popularProducts, setPopularProduct] = useState([]);
   useEffect(()=>{
     fetch('https://styleboom-c.onrender.com/popularinwomen')
-    .then((response)=>{console.log("data ye:",data);response.json()})
+    .then((response)=>response.json())
     .then((data)=>{
       console.log("Data: ", data);
-      
       setPopularProduct(data)})
   },[])
   return (
