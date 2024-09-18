d389e0d6b11e1d4330d8701b7e948443e5c44b9e
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-18 10:19:25 +0530

    jherii

--- backend/index.js
+++ backend/index.js
@@ -233,6 +233,7 @@ app.get('/popularinwomen',async (req,res)=>{
     let products = await Product.find({category:"women"});
     let popular_in_women = products.slice(0,4);
     console.log("Popular in women fetched");
+    console.log(popular_in_women);
     res.send(popular_in_women);
     
 })
