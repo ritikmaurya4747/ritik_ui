d2f7dc8878d4f72d6b75688a3de3f83a56f54e32
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-09-18 10:31:48 +0530

    Add console .log

--- backend/index.js
+++ backend/index.js
@@ -232,9 +232,10 @@ app.get('/newcollection',async (req,res)=>{
 app.get('/popularinwomen',async (req,res)=>{
     let products = await Product.find({category:"women"});
     let popular_in_women = products.slice(0,4);
-    console.log("Popular in women fetched");
     console.log(popular_in_women);
+    console.log("Popular in women fetched");
     res.send(popular_in_women);
+    console.log(popular_in_women);
     
 })
 
